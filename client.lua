d63faa17f06a2cce6f2acb411c91722bf1f5919c
gsLukas <149729834+gsLukao@users.noreply.github.com>
2024-04-05 05:35:54 -0700

    Add files via upload
    Português 🇧🇷
    O código implementa um sistema de sorteio de recompensas para jogadores em um servidor de jogo. A lógica é a seguinte:
    
    A função emP.checkPayment() verifica se o jogador possui uma ficha necessária para participar do sorteio.
    Um número aleatório entre 1 e 100 é gerado para determinar o prêmio do jogador.
    Com base no número sorteado, o jogador pode ganhar dinheiro, itens, veículos ou armas.
    Uma thread é criada para fornecer ao jogador uma ficha a cada hora de jogo.
    O jogador é notificado sobre o prêmio recebido.
    Em suma, o código cria um sistema interativo que incentiva a participação dos jogadores, oferecendo-lhes a oportunidade de ganhar prêmios variados em um sorteio realizado periodicamente.
    
    English 🇺🇸
    The code implements a reward drawing system for players on a game server. The logic is as follows:
    
    The emP.checkPayment() function verifies if the player has a necessary token to participate in the drawing.
    A random number between 1 and 100 is generated to determine the player's prize.
    Based on the drawn number, the player can win money, items, vehicles, or weapons.
    A thread is created to provide the player with a token every hour of gameplay.
    The player is notified about the received prize.
    In summary, the code creates an interactive system that encourages player participation by offering them the chance to win various prizes in a periodically conducted drawing.

---
+++ client.lua
@@ -0,0 +1,53 @@
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+Tunnel.bindInterface("sorte_revez")
+emP = Tunnel.getInterface("sorte_revez")
+
+local processo = false
+local segundos = 1
+
+RegisterCommand("box", function()
+    gsales()
+end)
+
+function gsales()
+    if not processo then
+        if emP.checkPayment() then
+            processo = true
+            TriggerEvent("Notify", "success", "Você tentou a sorte!") -- Notificação de sucesso
+            print('GSLUKÂO| Comunidade')
+        end
+    else
+        local tempo_restante = segundos / 60 -- Converte segundos para minutos
+        local minutos = math.floor(tempo_restante)
+        local segundos_restantes = math.floor((tempo_restante - minutos) * 60)
+        local mensagem = string.format("Você já tentou a sorte recentemente. Tente novamente depois!.", minutos, segundos_restantes)
+        TriggerEvent("Notify", "aviso", mensagem) -- Notificação de aviso com o tempo restante
+    end
+end
+
+
+RegisterNetEvent('notificarItemFicha')
+AddEventHandler('notificarItemFicha', function()
+    -- Código para exibir a notificação ao jogador
+    TriggerEvent('chat:addMessage', {
+        color = {255, 0, 0},
+        multiline = true,
+        args = {"[Servidor]", "Você recebeu uma ficha!"}
+    })
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        if segundos > 0 then
+            segundos = segundos - 1
+            if segundos == 0 then
+                processo = false
+            end
+        end
+    end
+end)
+
